bada1d470efb5a6c0930526b937ac88265154398
Wutchara Rachadach <wutchara.rachadach@refinitiv.com>
2020-02-22 20:29:47 +0700

    Add collection preview for shops pages

---
+++ crwn-clothing/src/components/preview-collection/preview-collection.component.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './preview-collection.component.scss';
+import CollectionItem from '../collection-item/collention-item.component';
+
+const PreviewCollection = ({ title, items }) => (
+  <div className='collection-preview'>
+    <h1 className='title'>{title.toUpperCase()}</h1>
+    <div className='preview'>
+      {items
+        .filter((_, idx) => idx < 4)
+        .map(({ id, ...otherItemProps }) => (
+          <CollectionItem key={id} {...otherItemProps} />
+        ))}
+    </div>
+  </div>
+);
+
+export default PreviewCollection;
