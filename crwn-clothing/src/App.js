7fd1a2dfea677ad352a0e0258fbad6389624d11c
Wutchara Rachadach <wutchara.rachadach@refinitiv.com>
2020-02-23 10:58:29 +0700

    Add sign in page and create new components for input

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -4,6 +4,7 @@ import HomePage from './pages/homepage/homepage.component';
 import {Switch,Route} from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Headers from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 const HatsPage = () => (
   <div>
@@ -17,6 +18,7 @@ function App() {
     <Switch>
       <Route exact path='/' component={HomePage} />
       <Route path='/shop' component={ShopPage} />
+      <Route path='/signin' component={SignInAndSignUpPage}/>
     </Switch>
   </div>;
 }
